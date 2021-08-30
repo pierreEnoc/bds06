72a7524b8943e2288155559d4127cdb86cb756c8
pierreEnoc <pierre_email@yahoo.fr>
2021-08-30 12:34:49 -0300

    :sparkles: add genre, movie repositories

--- src/main/java/com/devsuperior/movieflix/entities/Genre.java
+++ src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -3,10 +3,23 @@ package com.devsuperior.movieflix.entities;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_genre")
 public class Genre {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
+	@OneToMany(mappedBy = "movies")
 	private Set<Movie> movies = new HashSet<>();
 	
 	public Genre() {
